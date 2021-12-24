56867afec44d8fbd72420c7e4bf6325a4a15c8c6
Kit139 <2514154656@qq.com>
2021-12-25 00:45:57 +0800

    datas

--- get_datas.py
+++ get_datas.py
@@ -1,30 +1,33 @@
-import datetime
-
-import requests
-import execjs
+import datetime,requests,execjs,time,random,jsonpath
+import json
 import pandas as pd
+from os.path import exists
+from os import makedirs
+from fake_useragent import UserAgent
+from urllib.request import quote
 
+RESULTS_DIR = 'results'
+exists(RESULTS_DIR) or makedirs(RESULTS_DIR)
 
 # 搜索指数数据解密
 def decryption(keys, data):
     dec_dict = {}
     for j in range(len(keys) // 2):
         dec_dict[keys[j]] = keys[len(keys) // 2 + j]
-
     dec_data = ''
     for k in range(len(data)):
         dec_data += dec_dict[data[k]]
     return dec_data
 
-def get_datas(d,scenicName,y,d_name):
-    dataUrl = 'https://index.baidu.com/api/SearchApi/index?area={}&word=[[%7B%22name%22:%22{}%22,%22wordType%22:1%7D]]&startDate={}-01-02&endDate={}-01-01'.format(d,scenicName,y,y+1)
-    keyUrl = 'https://index.baidu.com/Interface/ptbk?uniqid='
+def get_res(url):
+    requests.adapters.DEFAULT_RETRIES = 5
     header = {
         'Accept': 'application/json, text/plain, */*',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Connection': 'keep-alive',
-        'Cookie': 'BDUSS=pUVVRsazRNV0xBb2V1ekVXdlFQZlc1WkhoY05jZGF-THR6RFZKd3VJbGlQc3RoSVFBQUFBJCQAAAAAABAAAAEAAABsQD~7bG92ZXNjcmFweQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGKxo2FisaNhTn; BDUSS_BFESS=pUVVRsazRNV0xBb2V1ekVXdlFQZlc1WkhoY05jZGF-THR6RFZKd3VJbGlQc3RoSVFBQUFBJCQAAAAAABAAAAEAAABsQD~7bG92ZXNjcmFweQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGKxo2FisaNhTn; __yjs_duid=1_3e0e0d21420e02edd28e3957e214d5d81638369910940; PSTM=1638440151; BIDUPSID=2B5A58CD9B34C75B73927D1173691AAB; BAIDUID=9075CEBBDB61149A821A686582441B6A:FG=1; BAIDUID_BFESS=ED34EE7BA6EA26D0E0D075C689E263A5:FG=1; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; H_PS_PSSID=34443_35292_35104_31253_35239_34968_34584_35490_35532_35233_26350_35478; Hm_lvt_d101ea4d2a5c67dab98251f0b5de24dc=1640241464; bdindexid=fgh0b7raeif6vv7uutc981o777; Hm_lpvt_d101ea4d2a5c67dab98251f0b5de24dc=1640241483; __yjs_st=2_ODYzMjNkNTNkNDkxNTU5Yzk0NzE4NDg4YThmMDUxNTFkNWM0YjliMzQyNjFiOTUxODVjNDdkMmU2MWM5MDY3MGYzNjRlNGIzM2I4ZTYzNGRlMWI4NmUzNjgxY2YxY2I5NWNhM2Q3N2VjMGI0NzE5NTM2ZTFiOTYxMjJmMTkzZGQ1MWE0ZTY4MDlkZGU2ZDQ2OWIyMGJhYmYzZTY0MzgwYWNkZjliNDJiMDY1M2M0NWI4YmJmYThiOWYwYWNiNWM5MzkxMjFmMzE1MzE3NWUzZTA5Mjk2OTMyMjJjNDJmOGY3YWViNTc4Yjc0ZThkZjE4MDA1MDIwOTJjNDIxOGJjOV83XzNjN2E2ZDMz; RT="z=1&dm=baidu.com&si=fplw3nxjjyk&ss=kxj0hz4z&sl=2&tt=3q3&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&ld=31sy"',
+        # 'Connection': 'keep-alive',
+        'Connection': 'close',
+        'Cookie': 'BDUSS=pUVVRsazRNV0xBb2V1ekVXdlFQZlc1WkhoY05jZGF-THR6RFZKd3VJbGlQc3RoSVFBQUFBJCQAAAAAABAAAAEAAABsQD~7bG92ZXNjcmFweQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGKxo2FisaNhTn; BDUSS_BFESS=pUVVRsazRNV0xBb2V1ekVXdlFQZlc1WkhoY05jZGF-THR6RFZKd3VJbGlQc3RoSVFBQUFBJCQAAAAAABAAAAEAAABsQD~7bG92ZXNjcmFweQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGKxo2FisaNhTn; __yjs_duid=1_3e0e0d21420e02edd28e3957e214d5d81638369910940; PSTM=1638440151; BIDUPSID=2B5A58CD9B34C75B73927D1173691AAB; BAIDUID=9075CEBBDB61149A821A686582441B6A:FG=1; BAIDUID_BFESS=ED34EE7BA6EA26D0E0D075C689E263A5:FG=1; Hm_lvt_d101ea4d2a5c67dab98251f0b5de24dc=1640241464; __yjs_st=2_ODYzMjNkNTNkNDkxNTU5Yzk0NzE4NDg4YThmMDUxNTFkNWM0YjliMzQyNjFiOTUxODVjNDdkMmU2MWM5MDY3MGYzNjRlNGIzM2I4ZTYzNGRlMWI4NmUzNjgxY2YxY2I5NWNhM2Q3N2VjMGI0NzE5NTM2ZTFiOTYxMjJmMTkzZGQ1MWE0ZTY4MDlkZGU2ZDQ2OWIyMGJhYmYzZTY0MzgwYWNkZjliNDJiMDY1M2M0NWI4YmJmYThiOWYwYWNiNWM5MzkxMjFmMzE1MzE3NWUzZTA5Mjk2OTMyMjJjNDJmOGY3YWViNTc4Yjc0ZThkZjE4MDA1MDIwOTJjNDIxOGJjOV83XzNjN2E2ZDMz; ab_sr=1.0.1_MWM0YmYwNzEzNDQwZjFmZjA4ZDEwMzZiNTA2MDQ0Y2E0ZWMxMTU0MWY2NmY4OGVhYjAxZjFhNGM2NDczMzNmZjhkNDA2NDJiMWNhMWY5MTE5ZDFiYmNlZjVlYTViOTk4Mjg0MDRhZjViMTljZjQ5NjAzOGE2MWI1ZWMxOTZhNzJiZTU4NzY2ZGM1OTgyODk1ZGQ3N2YwYjliNjU5OGU2Mw==; bdindexid=oaim5ilmve01plumh8j13mheb6; RT="z=1&dm=baidu.com&si=mqrdx06prj9&ss=kxk5bhe9&sl=2&tt=7ha&bcn=https://fclog.baidu.com/log/weirwood?type=perf&ld=6mt&ul=kum"',
         'Host': 'index.baidu.com',
         'Referer': 'https://index.baidu.com/v2/main/index.html',
         'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
@@ -32,45 +35,58 @@ def get_datas(d,scenicName,y,d_name):
         'Sec-Fetch-Dest': 'empty',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-origin',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
+        # 'User-Agent': UserAgent(use_cache_server=False).random,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62',
     }
-    # 设置请求超时时间为30秒
-    resData = requests.get(dataUrl, timeout=30, headers=header)
+    try:
+        res = requests.get(url,headers=header, timeout=30)
+        if res.status_code == 200:
+            return res
+    
+    except requests.RequestException:
+        print('error occurred while scraping %s', url)
 
+
+def get_datas(d_index,word,y,d_name,keys):
+    dataUrl = 'https://index.baidu.com/api/SearchApi/index?area={}&word={}&startDate={}-01-01&endDate={}-12-31'.format(d_index,word,y,y)
+    keyUrl = 'https://index.baidu.com/Interface/ptbk?uniqid='
+
+    resData = get_res(dataUrl)
     uniqid = resData.json()['data']['uniqid']
     print("uniqid:{}".format(uniqid))
-    keyData = requests.get(keyUrl + uniqid, timeout=30, headers=header)
+
+    keyData = get_res(keyUrl + uniqid)
     keyData.raise_for_status()
     keyData.encoding = resData.apparent_encoding
 
-    # 开始对json数据进行解析
     startDate = resData.json()['data']['userIndexes'][0]['all']['startDate']
-    print("startDate:{}".format(startDate))
     endDate = resData.json()['data']['userIndexes'][0]['all']['endDate']
-    print("endDate:{}".format(endDate))
-    source = (resData.json()['data']['userIndexes'][0]['all']['data'])  # 原加密数据
-    # print("原加密数据:{}".format(source))
+    print("{},{},{}".format(d_name,startDate,endDate))
+
+    sources = jsonpath.jsonpath(resData.json(),'$..all.data')
     key = keyData.json()['data']  # 密钥
-    # print("密钥:{}".format(key))
-
-
-    res = decryption(key, source)
-    resArr = res.split(",")
 
     dateStart = datetime.datetime.strptime(startDate, '%Y-%m-%d')
     dateEnd = datetime.datetime.strptime(endDate, '%Y-%m-%d')
     dataLs = []
     while dateStart <= dateEnd:
-        dataLs.append(str(dateStart))
+        dataLs.append(str(dateStart)[0:10])
         dateStart += datetime.timedelta(days=1)
 
-    ls = []
-    for i in range(len(dataLs)):
-        ls.append([d_name, scenicName, dataLs[i], resArr[i]])
-
-    # for i in range(len(ls)):
-    #     print(ls[i])
-    return pd.DataFrame(ls,columns=['areas','keys','times','keys'])
+    df = pd.DataFrame()
+    df['area'] = [d_name for i in range(len(dataLs))]
+    df['time'] = dataLs
+    for source,ke in zip(sources,keys):
+        res = decryption(key, source)
+        resArr = res.split(",")
+        lenth1 = len(dataLs)
+        # lenth2 = len(resArr)
+        if resArr == ['']:
+            resArr = ['0' for i in range(lenth1)]
+        # elif 1 <lenth2<lenth1:
+        #     resArr = ['0' for i in range(lenth1-lenth2)]+resArr
+        df[ke] = [0 if i == '' else i for i in resArr]
+    return df
 
 if __name__ == "__main__":
     areas = {'北京':911,'天津':923,'河北':920}
@@ -78,11 +94,20 @@ if __name__ == "__main__":
     for d in areas.items():
         d_name = d[0]
         d_index = d[1]
-        print(d_name)
-        df = pd.DataFrame(columns=['areas','keys','times','keys'])
-        keys = ['第三方支付','人工智能','P2P']
-        for k in keys:           
-            for y in range(2011,2021):
-                df1=get_datas(d_index,k,y,d_name)
-                df = pd.concat([df,df1])
-        df.to_csv('./results/{}.csv'.format(d_name), index=False)
+        df = pd.DataFrame(columns=['area','time'])
+        keys = ['大数据','人工智能','区块链','第三方支付','在线支付','移动支付','网上银行','电子银行','网银','互联网理财','互联网保险','众筹','P2P','网贷']
+        ky=10
+        keys = keys[ky::]
+
+        words = '[]]'
+        # keys = ['大数据','人工智能']
+        for k in keys:
+            words = words.replace(']]','[%7B%22name%22:%22'+quote(k)+'%22,%22wordType%22:1%7D],]]')
+        word = words.replace('],]]',']]')
+        for y in range(2011,2021):
+            df1=get_datas(d_index,word,y,d_name,keys)
+            df = pd.concat([df,df1])
+            ts = random.uniform(2.5,5.3)
+            print('要睡{}s'.format(ts))
+            time.sleep(ts)
+        df.to_csv('./results/{}_{}.csv'.format(d_name,ky), index=False)
